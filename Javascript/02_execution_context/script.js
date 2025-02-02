16a0249021852bbda78a88a2663f88a1a5932eec
Roshan kumar bhagat <roshanking021@gmail.com>
2025-02-02 23:31:07 +0530

    learned about hoisting and call stack

---
+++ Javascript/02_execution_context/script.js
@@ -0,0 +1,29 @@
+// debugger
+// var userName = "Roshan"
+// const userAge = 20
+
+// function sayHi(){
+//     var a =20
+//     var b = 10
+//     add(a,b)
+//     kuchBhi()
+//   return typeof(a+b)
+// }
+
+// function add(){
+//     console.log(5+6)
+//     add()
+// }
+
+// add()
+// console.log("Program ended")
+
+// function kuchBhi(){
+//     console.log("Hello")
+// }
+
+// sayHi()
+
+let  userName = "Roshan"
+let name2 = userName
+
