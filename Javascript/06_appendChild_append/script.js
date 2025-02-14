0445c95595da61c1869a02cc507f89750542d7ca
Roshan kumar bhagat <roshanking021@gmail.com>
2025-02-14 10:44:28 +0530

    complected the hero card section

--- Javascript/06_appendChild_append/script.js
+++ Javascript/06_appendChild_append/script.js
@@ -2,16 +2,16 @@ const container =  document.querySelector(".container")
 const card = document.querySelector(".card")
 
 
-// for (let i = 2; i<=100; i++){
-//     const duplicateCard = card.cloneNode()
-//     if (i%2){
-//         duplicateCard.classList.add("even")
-//     }else{
-//         duplicateCard.classList.add("odd")
-//     }
+for (let i = 2; i<=100; i++){
+    const duplicateCard = card.cloneNode()
+    if (i%2){
+        duplicateCard.classList.add("even")
+    }else{
+        duplicateCard.classList.add("odd")
+    }
     
-//     duplicateCard.textContent = i
-//     container.append(duplicateCard)
-// }
+    duplicateCard.textContent = i
+    container.append(duplicateCard)
+}
 
-// card.appendChild(container)
+card.appendChild(container)
--- css/projects/zomato_landing_page/script.js
+++ css/projects/zomato_landing_page/script.js
@@ -3,7 +3,6 @@ const menuIconElement = document.querySelector("#menu-icon");
 const closeIconElement = document.querySelector("#close-btn");
 
 menuIconElement.addEventListener("click", () => {
-    console.log("hello");
     responsiveMenu.classList.toggle("open"); // Add the "open" class to show the menu
 });
 
