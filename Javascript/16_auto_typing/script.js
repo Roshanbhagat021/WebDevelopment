4b60ae809f7d08c93d19669f2a865cb9e981cf1d
Roshan kumar bhagat <roshanking021@gmail.com>
2025-02-27 20:50:36 +0530

    auto typing

---
+++ Javascript/16_auto_typing/script.js
@@ -0,0 +1,34 @@
+const span  = document.querySelector("span");
+const p  = document.querySelector("h3 span");
+
+
+const wordsList = ["Developer","Coder","Learner","Passionate"]
+const wordsList2 = ["Student","Roshan","Brother","Son"]
+
+autoTyping(wordsList,span)
+autoTyping(wordsList2,p)
+
+function autoTyping(wordsList,element){
+    let characterIndex = 0;
+    let wordIndex = 0
+    let reverseType = false
+    setInterval(() => {
+        if(element.innerText.length == wordsList[wordIndex].length){
+            reverseType = true
+        }
+        if(!reverseType){
+            element.innerText = `${element.innerText}${wordsList[wordIndex][characterIndex]}`;
+            characterIndex++;
+        }else if(reverseType){
+            element.innerText = `${element.innerText.slice(0,element.innerText.length-1)}`
+            if(element.innerText.length == 0){
+                reverseType = false
+                characterIndex = 0
+                wordIndex++
+                if(wordIndex==wordsList.length){
+                    wordIndex=0
+                }
+            }
+        }
+    }, 200);
+}
