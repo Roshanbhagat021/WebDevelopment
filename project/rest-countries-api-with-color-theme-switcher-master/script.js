14285383f4bcb9efebf1781931c9647c99153954
Roshan <roshanking021@gmail.com>
2025-03-07 23:26:04 +0530

    created a countries searching website with api

--- project/rest-countries-api-with-color-theme-switcher-master/script.js
+++ project/rest-countries-api-with-color-theme-switcher-master/script.js
@@ -1,9 +1,16 @@
 const countryContainer = document.querySelector(".country-container");
+const searchCountryInputBox = document.querySelector(".search-country");
+const filterHead = document.querySelector(".filter-head");
+const filterOptions = document.querySelector(".filter-options");
+const AllfilterOptions = document.querySelectorAll(".filter-options div");
 
 async function fetchCountryData() {
   try {
-    const fetchedData = await fetch("https://restcountries.com/v3.1/all");
-    // const fetchedData = await fetch("https://restcountries.com/v3.1/all?fields=name,flags,capital");
+    // const fetchedData = await fetch("https://restcountries.com/v3.1/all");
+    const fetchedData = await fetch(
+      "https://restcountries.com/v3.1/all?fields=name,flags,capital,cca3,population,region"
+    );
+    // const fetchedData = await fetch("https://restcountries.com/v3.1/name/india?fullText=true&fields=name,flags,capital,cca3,population,region");
     const data = await fetchedData.json();
     return data;
   } catch (error) {
@@ -11,56 +18,83 @@ async function fetchCountryData() {
   }
 }
 
+filterHead.addEventListener("click", () => {
+  filterOptions.classList.toggle("hidden");
+});
 
 
-function createCountryCard(countryInfo) {
-    console.log(countryInfo);
-  const linktosinglecountry = document.createElement("a") 
-  linktosinglecountry.href = `singlecountry.html?${countryInfo.cca3}`
 
+function createCountryCard({cca3,flags,name,population,region,capital}) {
+  const linktosinglecountry = document.createElement("a");
+  linktosinglecountry.href = `singlecountry.html?cca3=${cca3}`;
 
-  const countryCard = document.createElement("div");
-  countryCard.classList.add("country-card");
+  const cardHtml = `
+        <div class="country-card">
+          <img src="${flags.png}" alt="${flags.png}">
+          <div class="country-details">
+              <h3>${name.common}</h3>
+              <p><b>Poulation: </b><span>${population.toLocaleString()}</span></p>
+              <p><b>Region: </b><span>${region}</span></p>
+              <p><b>Capital:</b> ${capital[0]?capital[0]: "No Official Capital"}</span></p>
+          </div>
+        </div>`;
 
-  const flagImage = document.createElement("img");
-  flagImage.setAttribute("src", `${countryInfo.flags.png}`);
-
-  const countryDetails = document.createElement("div");
-  countryDetails.classList.add("country-details");
-
-  // const countryName = document.createElement("h3")
-  // countryName.innerText = "India"
-
-  // const population = document.createElement("p")
-  // population.innerText = "465416521"
-
-
-  const details = `
-                <h3>${countryInfo.name.common}</h3>
-                <p><b>Poulation: </b><span>${countryInfo.population}</span></p>
-                <p><b>Region: </b><span>${countryInfo.region}</span></p>
-                <p><b>Capital:</b> <span>${countryInfo.capital}</span></p>
-                    `;
-
-  countryDetails.innerHTML = details;
-  countryCard.append(flagImage, countryDetails);
-  linktosinglecountry.append(countryCard)
+  linktosinglecountry.innerHTML = cardHtml;
   countryContainer.append(linktosinglecountry);
 }
 
-// createCountryCard();
-// createCountryCard();
-// createCountryCard();
-// createCountryCard();
-// createCountryCard();    
 
+let countriesData = "";
 
 async function displayData() {
-    const data = await fetchCountryData()
-    data.forEach((country)=>{
-        // console.log(country.flags.svg);
-        createCountryCard(country)
-    })
+  const data = await fetchCountryData();
+  countriesData = data;
+  data.forEach((country) => {
+    // console.log(country);
+    createCountryCard(country);
+  });
 }
 
-displayData()
+searchCountryInputBox.addEventListener("input", () => {
+  const regionData = countriesData.reduce((acc, el) => {
+    acc[el.region] = (acc[el.region] || 0) + 1;
+    return acc;
+  }, {});
+  console.log(regionData);
+  countryContainer.innerHTML = "";
+  let filteredData = countriesData.filter((country) => {
+    return country.name.common
+      .toLowerCase()
+      .includes(searchCountryInputBox.value.toLowerCase());
+  });
+  console.log(filteredData);
+  filteredData.forEach((country) => {
+    // console.log(country);
+    createCountryCard(country);
+  });
+});
+
+function filterByRegion(rg) {
+  countryContainer.innerHTML = "";
+  let filteredData = countriesData.filter((country) => {
+    return country.region.toLowerCase() == rg.toLowerCase();
+  });
+  filteredData.forEach((country) => {
+    createCountryCard(country);
+  });
+}
+
+AllfilterOptions.forEach((option) => {
+  option.addEventListener("click", (e) => {
+    if (e.target.innerText == "All") {
+      countryContainer.innerHTML = "";
+      countriesData.forEach((country) => {
+        createCountryCard(country);
+      });
+      return;
+    }
+    filterByRegion(e.target.innerText);
+  });
+});
+
+displayData();
--- project/rest-countries-api-with-color-theme-switcher-master/script2.js
+++ project/rest-countries-api-with-color-theme-switcher-master/script2.js
@@ -1,12 +1,13 @@
 const extendedInfo = document.querySelector(".extended-details")
-const countryCode = window.location.search.slice(1)
+// const countryCode = window.location.search.slice(6)
+const countryCode = new URLSearchParams(location.search).get("cca3")
 
-async function fetchCountryData() {
+
+async function fetchCountryData(cc) {
     try {
-      const fetchedData = await fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`);
+      const fetchedData = await fetch(`https://restcountries.com/v3.1/alpha/${cc}`);
       // const fetchedData = await fetch("https://restcountries.com/v3.1/all?fields=name,flags,capital");
       const data = await fetchedData.json();
-    //   console.log(data[0]);
       return data;
     } catch (error) {
       console.log(error);
@@ -14,33 +15,44 @@ async function fetchCountryData() {
   }
   
 
-  fetchCountryData()
 
+async function createDataContainer(data){
+  // console.log('data: ', data.name,Object.values(data.name.nativeName)[0].official);
+    let borderCountries = ""
+    if (data?.borders) {
+      const borderPromises = data.borders.map(async (ccode) => {
+        const borderData = await fetchCountryData(ccode);
+        // console.log(borderData[0].name.common);
+        return `<a href="singlecountry.html?cca3=${borderData[0].cca3}"><span>${borderData[0].name.common}</span></a>`;
+      });
+      borderCountries = (await Promise.all(borderPromises)).join('');
+    }
+    // console.log(borderCountries);
 
-function createDataContainer(data){
-    console.log(data.currencies[Object.keys(data.currencies)[0]].name);
+    
 
-    const container = `<img src=${data.flags.png} alt="">
-        <div class="more-details">
-            <h2>${data.name.common}</h2>
-            <div class="regional-details">
-                <div class="first-part">
-                    <p><b>Native Name: </b>${data.name.official}</p>
-                    <p><b>Population: </b>${data.population}</p>
-                    <p><b>Region: </b>${data.region}</p>
-                    <p><b>Sub Region: </b>${data.subregion}</p>
-                    <p><b>Capital: </b>${data.capital}</p>
-                </div>
-                <div class="second-part">
-                    <p><b>Top Level Domain: </b>${data.tld}</p>
-                    <p><b>Currencies: </b>${data.currencies[Object.keys(data.currencies)[0]].name}</p>
-                    <p><b>Languages: </b>${Object.values(data.languages)}</p>
-                </div>
-            </div>
-            <div class="border-countries">
-                <p><b>Border Countries: </b><span>France</span><span>Germany</span><span>Netherlands</span></p>
-            </div>
-        </div>`
+    const container = `
+      <img src="${data.flags.png}" alt="Flag of ${data.name.common}">
+      <div class="more-details">
+        <h2>${data.name.common}&nbsp; <a title="Open to Wikipedia" target="_blank" style="color:blue;" href="https://en.wikipedia.org/wiki/${data.name.common}"><i class="fa-solid fa-arrow-up-right-from-square"></i></a></h2>
+        <div class="regional-details">
+          <div class="first-part">
+            <p><b>Native Name:</b> ${data.name.nativeName?Object.values(data.name.nativeName)[0].official:data.name.common}</p>
+            <p><b>Population:</b> ${data.population.toLocaleString()}</p>
+            <p><b>Region:</b> ${data.region}</p>
+            <p><b>Sub Region:</b> ${data.subregion?data.subregion:"No Official Subregion"}</p>
+            <p><b>Capital:</b> ${data.capital ? data.capital : "No Official Capital"}</p>
+          </div>
+          <div class="second-part">
+            <p><b>Top Level Domain:</b> ${data.tld.join(', ')}</p>
+            <p><b>Currencies:</b> ${data.currencies ? Object.values(data.currencies).map(currency => currency.name).join(', ') : "No Official Currency"}</p>
+            <p><b>Languages:</b> ${data.languages ? Object.values(data.languages).join(', ') : "No Official Language"}</p>
+          </div>
+        </div>
+        <div class="border-countries">
+          <p><b>Border Countries:</b> ${borderCountries ? borderCountries : "Country has no bordering nations"}</p>
+        </div>
+      </div>`;
 
         extendedInfo.innerHTML = container
 }
@@ -48,9 +60,9 @@ function createDataContainer(data){
 
 
   async function displayData() {
-    const data = await fetchCountryData()
-    createDataContainer(data[0])
-    
+    const [data] = await fetchCountryData(countryCode)
+    document.title = data.name.common
+    createDataContainer(data)
 }
 
 displayData()
